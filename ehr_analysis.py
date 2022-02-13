2fb8318f6fbe484955245b429fb0619598656666
Ruizhi Yuan <94030641+yrzzz@users.noreply.github.com>
2022-02-13 16:44:41 -0500

    Update part2

---
+++ ehr_analysis.py
@@ -0,0 +1,111 @@
+from datetime import datetime
+
+"""
+I choose to read the data as a list. As we need to analyze the data in the dataset, a list is a good choice since
+we can locate each element in a list of lists.
+"""
+
+
+def parse_data(filename: str) -> list[list[str]]:
+    """
+    For this function, open a file takes one operation. If the file has N lines, reading each line takes N operations.
+    Setting up a empty list takes one operation. Inside the for loop, there is 3 operations, so the whole loop takes 3N
+    operations. Return takes 1 operation. So the whole function takes 4n+3 steps and the complexity is O(N).
+    """
+    with open(filename, "r") as file:
+        rows = file.readlines()
+        lists = []
+        for row in rows:
+            row = row.strip()
+            row = row.split("\t")  # type: ignore
+            lists.append(row)
+        return lists
+
+
+def num_older_than(age: float, data: list[list[str]]) -> int:
+    """
+    This function receive a number and a list of lists. Setting the time of today takes one operation. Setting the
+    counting number takes one operation. In side the for loop, calculating the length of data minus 1 takes 2 operations
+    getting the range takes 1 operation. Locating the string takes 1 operation and transforming the string to date takes
+    1 operation. In the if statement, it takes 12 operations. Returning the result takes one operation. Thus, the total
+    steps we need is 14N+6. So the total computational complexity is O(N)
+    :param age: The age you want to compare.
+    :param data: The dataset like PatientCorePopulatedTable.txt.
+    :return: The number of people who is older than the given age.
+    """
+    today = datetime.today()
+    num = 0
+    for i in range(1, len(data)):
+        born = datetime.strptime(data[i][2], "%Y-%m-%d %H:%M:%S.%f")
+        if (
+            today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+        ) > age:
+            num += 1
+    return num
+
+
+def sick_patients(
+    lab: str, gt_lt: str, value: float, data: list[list[str]]
+) -> list[str]:
+    """
+    Setting a empty takes one operation. In the for loop, calculating the length of data minus 1 takes 2 operations.
+    Getting the range takes 1 operation. Locating and comparing the lab name takes 2 operations. Comparing the notation
+    takes 1 operation. Floating, locating and comparing the value takes 2 operations. Locating and adding the id into
+    the set takes 2 operations. Returning the list of set takes 2 operations. Thus, the total number of the operations
+    is 8N+6. So the computational complexity is O(N)
+    :param lab: Lab name you want ot choose
+    :param gt_lt: < or >
+    :param value: The value of the lab
+    :param data: Dateset of the data like LabsCorePopulatedTable.txt
+    :return: The list of patient id that was regarded as sick
+    """
+    id = set()
+    for i in range(1, len(data)):
+        if data[i][2] == lab:
+            if gt_lt == ">":
+                if float(data[i][3]) > value:
+                    id.add(data[i][0])
+            elif gt_lt == "<":
+                if float(data[i][3]) < value:
+                    id.add(data[i][0])
+            else:
+                raise ValueError("Please input '<' or '>' in second argument")
+    return list(id)
+
+
+def first_admission_age(patient_data: list[list[str]], lab_data: [list[list[str]]]):
+    """
+    This function utilizes dictionary. First, construct a dictionary to store each patient ID as the key, and the list
+    of admission date as value. Then, find the first admission date. Finally, use the birth date in the patient level
+    data to calculate the age of first admission
+
+    :param patient_data: Patient level data
+    :param lab_data: Lab level data
+    :return: A dictionary which keys are patients' ID, values are the age at their first admission
+    """
+    first_ad_age = {}
+    result_dict = {}
+    for i in range(1, len(lab_data)):
+        dict_a = {lab_data[i][0]: lab_data[i][5]}
+        key = list(dict_a.keys())[0]
+        if key not in result_dict:
+            result_dict[key] = []
+            result_dict[key].append(
+                datetime.strptime(dict_a[key], "%Y-%m-%d %H:%M:%S.%f")
+            )
+        else:
+            result_dict[key].append(
+                datetime.strptime(dict_a[key], "%Y-%m-%d %H:%M:%S.%f")
+            )
+
+    for i in range(1, len(patient_data)):
+        patient_id = patient_data[i][0]
+        born = datetime.strptime(patient_data[i][2], "%Y-%m-%d %H:%M:%S.%f")
+        first_ad_date = min(result_dict[patient_id])
+        age = (
+            first_ad_date.year
+            - born.year
+            - ((first_ad_date.month, first_ad_date.day) < (born.month, born.day))
+        )
+        first_ad_age[patient_id] = age
+    return first_ad_age
---
+++ test_ehr_analysis.py
@@ -0,0 +1,127 @@
+from ehr_analysis import parse_data, num_older_than, sick_patients, first_admission_age
+
+
+def test_parse_data():
+    dat = [
+        ["PatientID", "AdmissionID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "1",
+            "URINALYSIS: RED BLOOD CELLS",
+            "1.8",
+            "rbc/hpf",
+            "1992-07-01 01:36:17.910",
+        ],
+    ]
+    assert parse_data("D:\Work\Duke\Biostat821\EHR cumulative project1/test.txt") == dat
+
+
+def test_num_older_than():
+    dat = [
+        [
+            "PatientID",
+            "PatientGender",
+            "PatientDateOfBirth",
+            "PatientRace",
+            "PatientMaritalStatus",
+            "PatientLanguage",
+            "PatientPopulationPercentageBelowPoverty",
+        ],
+        [
+            "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
+            "Male",
+            "1947-12-28 02:45:40.547",
+            "Unknown",
+            "Married",
+            "Icelandic",
+            "18.08",
+        ],
+        [
+            "64182B95-EB72-4E2B-BE77-8050B71498CE",
+            "Male",
+            "1992-01-18 19:51:12.917",
+            "African American",
+            "Separated",
+            "English",
+            "13.03",
+        ],
+    ]
+    assert num_older_than(40, dat) == 1
+
+
+def test_sick_patients():
+    dat = [
+        ["PatientID", "AdmissionID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "1",
+            "URINALYSIS: RED BLOOD CELLS",
+            "1.8",
+            "rbc/hpf",
+            "1992-07-01 01:36:17.910",
+        ],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "1",
+            "METABOLIC: GLUCOSE",
+            "103.3",
+            "mg/dL",
+            "1992-06-30 09:35:52.383",
+        ],
+    ]
+    assert sick_patients("URINALYSIS: RED BLOOD CELLS", ">", 1.5, dat) == [
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+    ]
+    assert sick_patients("METABOLIC: GLUCOSE", "<", 105, dat) == [
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+    ]
+    assert (
+        sick_patients("METABOLIC: GLUCOSE", "larger", 105, dat)
+        == "Please input '<' or '>'"
+    )
+
+
+def test_first_admission_age():
+    patient_data = [
+        [
+            "PatientID",
+            "PatientGender",
+            "PatientDateOfBirth",
+            "PatientRace",
+            "PatientMaritalStatus",
+            "PatientLanguage",
+            "PatientPopulationPercentageBelowPoverty",
+        ],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "Male",
+            "1947-12-28 02:45:40.547",
+            "Unknown",
+            "Married",
+            "Icelandic",
+            "18.08",
+        ],
+    ]
+
+    lab_data = [
+        ["PatientID", "AdmissionID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "1",
+            "URINALYSIS: RED BLOOD CELLS",
+            "1.8",
+            "rbc/hpf",
+            "1992-07-01 01:36:17.910",
+        ],
+        [
+            "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
+            "1",
+            "METABOLIC: GLUCOSE",
+            "103.3",
+            "mg/dL",
+            "1992-06-30 09:35:52.383",
+        ],
+    ]
+    assert first_admission_age(patient_data, lab_data) == {
+        "1A8791E3-A61C-455A-8DEE-763EB90C9B2C": 44
+    }
